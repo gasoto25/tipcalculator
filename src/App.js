deb22f283f16ecb1e343e0bcb1d91fa45fd04bb3
George Soto <gasoto@Georges-MacBook-Pro.local>
2021-03-12 14:36:54 -0700

    refactored App.js component removed render prop

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import "./App.css";
 import FormWrapper from "./components/FormWrapper";
-import CheckTotalForm from "./components/CheckTotalForm";
-import TipPercentageForm from "./components/TipPercentageForm";
-import FinalNumbers from "./components/FinalNumbers";
 
 const App = () => {
   return (
     <div className="backG container-fluid pt-3" style={{ height: "100vh" }}>
-      <FormWrapper
-        render={(
-          checkTotal,
-          setCheckTotal,
-          tipPercentage,
-          setTipPercentage,
-          finalTip
-        ) => {
-          return (
-            <>
-              <CheckTotalForm
-                checkTotal={checkTotal}
-                setCheckTotal={setCheckTotal}
-              />
-              <TipPercentageForm
-                tipPercentage={tipPercentage}
-                setTipPercentage={setTipPercentage}
-                percentOptions={[15, 25, 30, 55, 60, 78, 89]}
-              />
-              <FinalNumbers finalTip={finalTip} checkTotal={checkTotal} />
-            </>
-          );
-        }}
-      />
+      <FormWrapper />
     </div>
   );
 };
